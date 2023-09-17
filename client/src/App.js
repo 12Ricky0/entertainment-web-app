5fa63af2a903ee38509a372b132775bc7088ea1b
Richard Addo <ramankwah03@gmail.com>
2023-09-17 18:02:47 +0200

    url configured

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import axios from "axios";
 
 export const BookmarkContext = createContext()
-axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://entertainment-web-app-theta.vercel.app';
 
 
 function App() {
