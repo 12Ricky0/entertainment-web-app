87c95396b9302b15400445991cba9adcf192f705
Richard Addo <123232609+12Ricky0@users.noreply.github.com>
2023-10-03 20:40:31 +0200

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
-module.exports = async () => {
-  roots: ['<rootDir>/src'],
+
+ module.exports = {
+  // Specify the test environment (e.g., Node.js or jsdom for browser-like testing).
+  testEnvironment: 'node',
+
+  // A list of directories to search for test files.
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+
+  // An array of file extensions your tests use.
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+
+  // Other Jest options you may need (e.g., coverage, setupFiles).
+  // ...
+};
   return {
     verbose: true,
   };
-};
