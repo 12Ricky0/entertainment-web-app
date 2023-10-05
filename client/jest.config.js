ce393eee2f325f272618c19471782a7be000ccc1
Richard Addo <ramankwah03@gmail.com>
2023-10-05 19:28:49 +0200

    test modified

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,15 +1,7 @@
 
 
 module.exports = {
-    preset: 'ts-jest',
     roots: ['client/src/Components'],
-    transform: {
-        '^.+\\.tsx?$': [
-            'ts-jest',
-            {
-                tsconfig: 'tests/tsconfig.json',
-            },
-        ],
-    }
 }
+
 
